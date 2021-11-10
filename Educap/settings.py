9e29155a9f2b74b634b0a34a57034cc5132c4e9c
Juan Chalita <chalajr2@gmail.com>
2021-11-09 19:15:52 -0600

    .

--- Educap/settings.py
+++ Educap/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-p1nr7y+=8jizau=nogae=uj1yr#$n0b=mo(=e81e56!91tyuz=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
